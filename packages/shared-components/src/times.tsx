c115a315c4aedcc23820aacbadc9a3815453b5ff
Aaron <aaron@sirrom.systems>
2025-02-14 20:19:50 -0700

    make header time look better

--- packages/shared-components/src/times.tsx
+++ packages/shared-components/src/times.tsx
@@ -13,5 +13,9 @@ export function Times() {
     return () => clearInterval(interval);
   });
 
-  return <>{time.toString()}</>;
+  return (
+    <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight self-center">
+      {time.toString()}
+    </h3>
+  );
 }
